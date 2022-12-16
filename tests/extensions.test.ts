360c54181d53a5aa805ab21a3b0e5c4d98433313
Alexander Mauri <maurialex@web.de>
2022-12-16 07:33:56 +0100

    register transient class test

---
+++ tests/extensions.test.ts
@@ -0,0 +1,65 @@
+import { ServiceCollection } from "@amaic/dijs";
+import "../src";
+
+describe("extensions", () =>
+{
+    test("register transient class", () =>
+    {
+        const serviceCollection = new ServiceCollection();
+
+        serviceCollection.RegisterTransientClass<IFoo, typeof Foo>(IFooIdentifier, Foo);
+
+        serviceCollection.RegisterTransientClass<IBar, typeof Bar>(IBarIdentifier, Bar, (ct, sp) =>
+            new ct(sp.GetRequiredService<IFoo>(IFooIdentifier))
+        );
+
+        const serviceProvider = serviceCollection.CreateServiceProvider();
+
+        const bar = serviceProvider.GetRequiredService<IBar>(IBarIdentifier);
+
+        expect(bar).toBeInstanceOf(Bar);
+
+        expect(bar.BarTest()).toBe("bar");
+
+        expect(bar.Foo).toBeInstanceOf(Foo);
+
+        expect(bar.Foo.FooTest()).toBe("foo");
+    });
+
+});
+
+const IFooIdentifier = Symbol();
+interface IFoo
+{
+    FooTest(): string;
+}
+class Foo implements IFoo
+{
+    FooTest(): string
+    {
+        return "foo";
+    }
+
+}
+
+const IBarIdentifier = Symbol();
+interface IBar
+{
+    BarTest(): string;
+
+    get Foo(): IFoo;
+}
+class Bar implements IBar
+{
+    constructor(foo: IFoo) 
+    {
+        this.Foo = foo;
+    }
+
+    public readonly Foo: IFoo;
+
+    BarTest(): string
+    {
+        return "bar";
+    }
+}
